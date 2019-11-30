2574efe65f085b3cc44c53b22cb48fe92a09fb3c
Mert Tecimen <merttecimen@Mert-MacBook-Pro.local>
2019-12-01 00:13:31 +0300

    New sites

--- ParsingWords.py
+++ ParsingWords.py
@@ -1,5 +1,5 @@
 import re
 f= open("../data_exapmle.txt","r")
 string_a = "yo what up, you are my dude!" # example string
-res = re.findall(r'\w+',f) # parsing words
-print(str(res)) # print
+res = re.findall(r'\w+',string_a) # parsing words
+print(str(res)) # printr
---
+++ techWordDetection.py
@@ -0,0 +1,8 @@
+
+def tWordDetect(data):
+    f = open(".../Open-Hack-Project/Open-Hack-Project/techWordDetect","r")
+    words = f.read()
+    
+
+
+    pass
