71066a58d132394a0407f193f541c55505337fd4
Mert Tecimen <merttecimen@Mert-MacBook-Pro.local>
2019-12-01 01:06:12 +0300

    New site

--- techWordDetection.py
+++ techWordDetection.py
@@ -1,8 +1,10 @@
 
-def tWordDetect(data):
+def tWordDetect(dataList):
     f = open(".../Open-Hack-Project/Open-Hack-Project/techWordDetect","r")
     words = f.read()
-    
+    for word in dataList:
+
+
 
 
     pass
