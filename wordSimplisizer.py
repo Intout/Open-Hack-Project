4c7dae7f6eb54f364a0cffa574f8be377f2d8559
Mustafa <Lopaev.mustafa79@gmail.com>
2019-12-01 13:30:12 +0300

    Update wordSimplisizer.py

---
+++ wordSimplisizer.py
@@ -0,0 +1,33 @@
+Vision-based lane detection is one of the most studied topics of the autonomous vehicle concept [2].
+most common problems encountered in vision-based lane detection studies are the varying distances between
+from these problems, computational costs are also a major problem in developing vision-based lane detection
+algorithms.
+Because of high computational costs, most lane detection algorithms presented in the literature are
+the problems mentioned above, real-time implementation of lane detection algorithms still constitutes a big
+In this study, the proposed lane detection algorithm is optimized in several ways to make it satisfactory
+Some of the lane detection algorithms presented in the literature are based on the randomized Hough
+SVM- and ANN-based lane detection algorithms have more computational costs compared to others.
+developed an ANN-based lane detection algorithm
+for detecting lane marks [8].
+determined the start position of the B-snake using the Canny/Hough estimation of the vanishing point algorithm.
+used the Kalman filter, which is commonly used for estimation, for lane detection [11].
+The proposed frame processing time in their algorithm was nearly 10 ms.
+programs to implement the methods developed in lane detection studies.
+used in these studies, such as the Hough transform, edge filtering, and many other image processing operations,
+Görüş tabanlı şerit tespit otonom araç kavramı [2] arasında en çok çalışılan konulardan biridir.
+vizyon temelli şeritli algılama çalışmalarında en sık karşılaşılan sorunlardır arasında değişen mesafeleri vardır
+Bu sorunların dışında, hesaplamalı maliyetler vizyon temelli şeritli algılama gelişmekte büyük bir sorun da
+algoritmaları.
+Çünkü yüksek hesaplama maliyetleri, literatürde sunulan en şerit algılama algoritmaları vardır
+yukarıda zikredilen problemler, şerit saptama algoritmaları, gerçek zamanlı uygulama hala büyük bir teşkil
+Bu çalışmada, önerilen şerit tespit algoritması tatmin edici hale getirmek için çeşitli şekillerde optimize
+Literatürde sunulan şeritli algılama algoritmaları bazıları randomize Hough dayanmaktadır
+SVM- ve YSA tabanlı şeritli algılama algoritmaları diğerlerine göre daha hesaplamalı maliyetleri var.
+YSA tabanlı şerit saptama algoritması gelişmiş
+Şerit işaretleri tespit etmek için [8].
+birleşme noktası algoritması tekin / Hough tahminini kullanarak B-yılan başlangıç ​​konumunu tespit edilmiştir.
+yaygın olarak şerit saptama [11] için, tahmin için kullanılan Kalman filtresi kullanılır.
+kendi algoritması önerilen kare işleme süresi yaklaşık 10 ms.
+programlar şeritli algılama çalışmalarında geliştirilen yöntemleri uygulamak.
+örneğin Hough transform, kenar filtreleme ve bir çok diğer görüntü işleme operasyonları gibi, bu çalışmalarda kullanılan,
+bulma
